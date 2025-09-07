ad2c359c040ca6cba5414a788482a59cfb67755c
unknown <urmilparsaniya4@gmail.com>
2025-09-07 11:50:09 +0530

    detail validation remove

--- routes/v1/assessment.ts
+++ routes/v1/assessment.ts
@@ -15,7 +15,7 @@ router
       body("title").trim().notEmpty().withMessage("Title is required"),
       body("date").trim().notEmpty().withMessage("Date is required"),
       body("location").trim().notEmpty().withMessage("Location is required"),
-      body("details").trim().notEmpty().withMessage("Details are required"),
+      // body("details").trim().notEmpty().withMessage("Details are required"),
       body("unit_ids").notEmpty().withMessage("Unit ID is required"),
       body("method_ids").notEmpty().withMessage("Method ID is required"),
       body("learner_id").notEmpty().withMessage("Learner Id is required")
